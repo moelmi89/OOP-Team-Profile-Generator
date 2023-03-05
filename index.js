7f56f0aa21ce9e0c0680fb26b250ce01abd2cdf0
Moelmi89 <moelmi89@gmail.com>
2023-03-05 22:33:41 +0000

    index paths and depends added. Render pg added

--- index.js
+++ index.js
@@ -2,3 +2,16 @@ const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+const jest = require("jest")
+
+let Team = [];
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRender");
+
