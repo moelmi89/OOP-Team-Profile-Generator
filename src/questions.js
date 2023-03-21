0402a21444bf9e9466a8b67f6e162900cc63599f
Moelmi89 <moelmi89@gmail.com>
2023-03-21 19:45:43 +0000

    question name changes for bug fix

--- src/questions.js
+++ src/questions.js
@@ -1,83 +1,83 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const initialQuestions = [{
-            type: 'list',
-            message: 'What would you like ot do next?',
-            name: 'selection',
-            choices: ['Add manager', 'Add engineer', 'Add intern'],
-        }
-    ];
-
-const ManagerQuestions = [{
-            type: "input",
-            message: "What is the manager's name? ",
-            name: "name"
-        },
-        {
-            type: "input",
-            message: "What is the manager's id? ",
-            name: "id"
-        },
-
-        {
-            type: "input",
-            message: "What is the manager's email? ",
-            name: "email"
-        },
-        {
-            type: "input",
-            message: "What is the manager's office number? ",
-            name: "officeNumber"
-
-        }
-    ];
-
-const EngineerQuestions = [{
-        type: "input",
-        message: "What is the engineer's name? ",
-        name: "name"
-    },
-    {
-        type: "input",
-        message: "What is the engineer's id? ",
-        name: "id"
-    },
-
-    {
-        type: "input",
-        message: "What is the engineer's email? ",
-        name: "email"
-    }, {
-
-        type: "input",
-        message: "What is the engineer's Github username? ",
-        name: "gitHub"
-    }
+const firstQuestions = [{
+    type: 'list',
+    message: 'What would you like ot do next?',
+    name: 'selection',
+    choices: ['Add manager', 'Add engineer', 'Add intern'],
+}
 ];
 
-
-const InternQuestions = [{
+const manQuestions = [{
     type: "input",
-    message: "What is the intern's name? ",
+    message: "What is the manager's name? ",
     name: "name"
 },
 {
     type: "input",
-    message: "What is the intern's id? ",
+    message: "What is the manager's id? ",
     name: "id"
 },
 
 {
     type: "input",
-    message: "What is the intern's email? ",
+    message: "What is the manager's email? ",
     name: "email"
-}, {
+},
+{
     type: "input",
-    message: "What is the intern's school? ",
-    name: "school"
+    message: "What is the manager's office number? ",
+    name: "officeNumber"
+
+}
+];
+
+const engQuestions = [{
+type: "input",
+message: "What is the engineer's name? ",
+name: "name"
+},
+{
+type: "input",
+message: "What is the engineer's id? ",
+name: "id"
+},
+
+{
+type: "input",
+message: "What is the engineer's email? ",
+name: "email"
+}, {
+
+type: "input",
+message: "What is the engineer's Github username? ",
+name: "gitHub"
+}
+];
+
+
+const intQuestions = [{
+type: "input",
+message: "What is the intern's name? ",
+name: "name"
+},
+{
+type: "input",
+message: "What is the intern's id? ",
+name: "id"
+},
+
+{
+type: "input",
+message: "What is the intern's email? ",
+name: "email"
+}, {
+type: "input",
+message: "What is the intern's school? ",
+name: "school"
 }
 
 ];
 
-module.exports = {initialQuestions, EngineerQuestions, InternQuestions, ManagerQuestions};
+module.exports = {firstQuestions, engQuestions, intQuestions, manQuestions};
